82117bcb7f79f2ac54353f1383235c48efec8aed
Zaim Aydin Nazif <119473291+zaimnazif974@users.noreply.github.com>
2026-06-01 15:07:34 +0700

    feat(sewagedung): done

--- backend/run_all.sh
+++ backend/run_all.sh
@@ -10,12 +10,12 @@ trap cleanup SIGINT
 
 read -p "Enter the path to the frontend directory: " frontend_dir
 
-echo "SELECT 'CREATE DATABASE reservationmanagement_product_ayoindonesia' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'reservationmanagement_product_ayoindonesia') \gexec" | psql "postgresql://:@localhost"
+echo "SELECT 'CREATE DATABASE reservationmanagement_product_sewagedung' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'reservationmanagement_product_sewagedung') \gexec" | psql "postgresql://:@localhost"
 for file in sql/*.sql; do
-    psql -a -f "$file" "postgresql://:@localhost/reservationmanagement_product_ayoindonesia"
+    psql -a -f "$file" "postgresql://:@localhost/reservationmanagement_product_sewagedung"
 done
 
-java -cp reservationmanagement.product.ayoindonesia --module-path reservationmanagement.product.ayoindonesia -m reservationmanagement.product.ayoindonesia &
+java -cp reservationmanagement.product.sewagedung --module-path reservationmanagement.product.sewagedung -m reservationmanagement.product.sewagedung &
 
 cd $frontend_dir && {
     npm install && {
